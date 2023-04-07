08c0bfb9273c688de8d41489266f00383f7aba4a
Arthur100024 <justfrog44@gmail.com>
2023-04-07 18:55:56 +0300

    Edit Functions.kt

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -5,13 +5,12 @@ fun main(args: Array<String>) {
 }
 
 
-fun sayHello(name: String){
+fun sayHello(name: String){ //? ФУНКЦИЯ С ПРИНИМАЕМЫМ ЗНАЧЕНИЕМ
     println("Hello, $name!")
-
 }
 
-fun addition(x:Int, y:Int): Int {
+fun addition(x:Int, y:Int): Int { //? ФУНКЦИЯ С ВЫОЗВРАЩАЕМЫМ ЗНАЧЕНИЕМ
     return x + y
 }
 
-fun saySomething(text: String): String = "Say $text"
+fun saySomething(text: String): String = "Say $text" //? ФУНКЦИЯ В ОДНУ СТРОКУ
